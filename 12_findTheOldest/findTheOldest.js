e4011c8f65c485f5ef1b4e619d7807c09bb984f0
John Bautista <jnl.bautista.cs@gmail.com>
2022-06-05 13:50:30 +0100

    problem 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson;
+  let maxYears = 0;
 
+  const d = new Date();
+
+  //   for (const person of people) {
+  //     if (person.hasOwnProperty("yearOfDeath") == false) {
+  //       person["yearOfDeath"];
+  //     }
+  //   }
+
+  for (const person of people) {
+    let yearsLived = 0;
+
+    if (person.hasOwnProperty("yearOfDeath") == false) {
+      yearsLived = d.getFullYear() - person.yearOfBirth;
+    }
+
+    if (person.hasOwnProperty("yearOfDeath") == true) {
+      yearsLived = person.yearOfDeath - person.yearOfBirth;
+    }
+
+    if (yearsLived > maxYears) {
+      maxYears = yearsLived;
+      oldestPerson = person;
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
