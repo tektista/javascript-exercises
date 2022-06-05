a3027f555c1a99c2f35f986a12f669ecd7a0ea8d
John Bautista <jnl.bautista.cs@gmail.com>
2022-06-05 12:58:11 +0100

    problem 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    
+  let a = 0,
+    b = 1,
+    c = Number(n);
 
+    if (c < 0) {
+        return "OOPS";
+    };
+
+  for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  return c;
 };
 
 // Do not edit below this line
