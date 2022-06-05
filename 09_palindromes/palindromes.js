58c95115b67768e96b4584dc88e225f08d223f73
John Bautista <jnl.bautista.cs@gmail.com>
2022-06-05 12:40:07 +0100

    problem 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  //lowercase
+  const stringLowerCase = string.toLowerCase();
 
+  //into array
+  const stringArray = stringLowerCase.split("");
+
+  const filteredArray = stringArray.filter(function (char) {
+    return char !== "!" && char !== "," && char !== "." && char !== " ";
+  });
+
+  //copy array by value
+  let filterArrayOriginal = [...filteredArray];
+
+  //reversed array
+  const reversedArray = filteredArray.reverse();
+
+  //into string
+  const filterArrayOriginals = filterArrayOriginal.join("");
+  const filteredString = filteredArray.join("");
+
+  if (filteredString == filterArrayOriginals) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
